fd92a98ad8ed5266a498d2e5cc45adba937b9835
Yuya Nishihara <yuya@tcha.org>
2022-01-03 16:37:32 +0900

    add highlight queries

---
+++ queries/locals.scm
@@ -0,0 +1,17 @@
+; Scopes
+
+; TODO: this is just a stub
+[
+  (binding_expression)
+  (callback_declaration)
+  (callback_connection)
+] @local.scope
+
+; Definitions
+
+(callback_connection_parameters
+  (identifier) @local.definition)
+
+; References
+
+(identifier) @local.reference
